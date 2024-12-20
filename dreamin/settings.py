e18fa7dff4d48347de74e63e4d9a75120a1150b4
muhammadkasyfil <141574422+muhammadkasyfil@users.noreply.github.com>
2024-12-20 17:53:00 +0700

    docker

--- dreamin/settings.py
+++ dreamin/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-dev-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
