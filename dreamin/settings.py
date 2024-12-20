d886e7dd752b8aa2a9f02e5265aa38e202225586
muhammadkasyfil <141574422+muhammadkasyfil@users.noreply.github.com>
2024-12-20 20:13:01 +0700

    what

--- dreamin/settings.py
+++ dreamin/settings.py
@@ -140,6 +140,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
