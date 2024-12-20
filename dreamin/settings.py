b58ad2bc18d9865df4a8de50cc257b388c840b18
muhammadkasyfil <141574422+muhammadkasyfil@users.noreply.github.com>
2024-12-20 20:05:47 +0700

    settings

--- dreamin/settings.py
+++ dreamin/settings.py
@@ -84,12 +84,23 @@ WSGI_APPLICATION = 'dreamin.wsgi.application'
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if DATABASE_URL:
+    # Production/Railway database
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Local database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
